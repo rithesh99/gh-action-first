867ff4602d3a62ce53fd78c97d61609468275c48
Rithesh D <ritheshdavid@gmail.com>
2025-01-30 16:48:35 +0530

    Added a react app

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('renders learn react link', () => {
+  it('pass test', () => {
+    expect(4+4).toEqual(8);
+  });
+
+  it('pass test 2', () => {
+    expect(4+4).toEqual(8);
+  });
+});
+
